596334e40147d71efb2e4fe8091b40258bfeb2dc
chiku <samugari@is.s.u-tokyo.ac.jp>
2015-03-10 02:17:07 +0900

    Integer infix parameter of Sharp-BQ reader macro is made optional.
    Sharp-Backquote reader macro from Let Over Lambda requires us to specify
    an integer infix parameter that denotes how many anaphoras should be
    introduced in the resulting lambda expression. I modified this behavior
    to make the parameter optional.
    
    The main difficulty was the treatment of comma. It is treated as a
    special strucutre SB-IMPL::COMMA in SBCL, while it is nothing special
    object in Clozure CL. I have not yet checked about the other
    implementations. In SBCL therefore, we have to strip the COMMA structure
    to properly collect the anaphoras.
    
    More accurately speaking, the reader macro behaves in a different manner
    if the optional integer parameter is given. If the parameter is not
    given, then the part that follows to #' is READ and the anaphoras are
    extracted. Anaphoras that appears in the expression is actually declared
    as the parameter of resulting lambda expression. Although anaphoras are
    sorted by the suffix, it can be desultory as follows:
    
        #`(list ,a7 a0) => (lambda (a0 a7) `(list ,a7 ,a0))
    
    If the optional integer parameter is given, on the other hand, it is
    recognized as the number of anaphoras. Even if some of introduced
    anaphoras not appear in the expression, it is introduced.
    
        #3`(list ,a0) => (lambda (a0 a1 a2) `(list ,a0))
    
    The suffix starts from 0. This is another point that differes from
    original version in Let Over Lambda.
    
    The intern timing of anaphoras is different between these 2 behaviors.
    READ function will interns the anaphoras in the former case while the
    reader macro explicitly interns anaphoras in the latter case. The
    package is not specified even in the latter case and thus it works as
    expected. Even if a symbol who can be considered as an anaphora was
    imported, both apperance (in parameter list and in expression) would
    refers an exactly same symbol.
    
    Any symbol whose SYMBOL-NAME is a member of following regular language
    is recognized as an anaphora:
    
        A0 | A[1-9][0-9]*
    
    Please be aware that the resolution is weakened since it used the
    SYMBOL-NAME to detect anaphoras. In other words, the package is not
    considered.
    
    I believe that the resolution should be retrieved, but I do not have a
    good approach to achive that.
    
    I realized that PAPPLY project holds a bug in ANAPHORA-LIST during this
    challenge.

---
+++ sharp-backquote.lisp
@@ -0,0 +1,68 @@
+;;;; Sharp backquote reader macro
+;;; Dec. 27th 2013, chiku
+;;; The #` reader macro introduced in Let Over the Lambda is an
+;;; excellent stuff. However, implicit insertion of anaphoras is more
+;;; preferable for me. Let me try to implement that variation.
+;;;  Here is the original one.
+;;; Mar. 10th 2015, chiku
+;;; Try again.
+(defun |#`-reader| (strm c n)
+  (declare (ignore c))
+  `(lambda ,(map0-n (lambda (n) (intern (format nil "A~A" n))) (1- (or n 1)))
+     ,(funcall (get-macro-character #\`) strm nil)))
+
+(defun anaphorap (sym)
+  " Retruns T if the name of the given symbol ``SYM'' is
+   a member of following regular language:
+   A[1-9][0-9]*|A0
+   "
+  (let ((name (symbol-name sym)))
+    (and (<= 2 (length name))
+         (string= (take name) "A")
+         (every #'digit-char-p (drop name))
+         (or (= (length name) 2)
+             ;; Disables padding 0
+             (not (char= (elt name 1) #\0))))))
+
+;;; For example, Clozure CL does not need this function. Unquoted
+;;; expressions are not something special structure.
+(defun unquoted-symbol-p (obj)
+  #+sbcl
+  (if (sb-impl::comma-p obj)
+    (let ((expr (sb-impl::comma-expr obj)))
+      (if (symbolp expr)
+        (values t expr)
+        (unquoted-symbol-p expr)))))
+
+(mapcar #'unquoted-symbol-p (flatten (read-from-string "'(lambda (x) `(print ,a))")))
+
+(mapcar #'unquoted-symbol-p (flatten (read-from-string "'(lambda (x) ``(print ,,a))")))
+
+(defun anaphora-list (tree)
+  " Returns a list that is composed of symbols whose ANAPHORAP check is
+   T. Each symbol appears only once in the list and the list is sorted
+   by the name.
+   "
+  (sort (remove-duplicates
+          (filter (lambda (obj)
+                    (or (and (symbolp obj) (anaphorap obj) obj)
+                        #+sbcl
+                        (multiple-value-bind (result expr)
+                          (unquoted-symbol-p obj)
+                          (and result expr))))
+                  (flatten tree)))
+        #'string<= :key #'symbol-name))
+
+(defun new-sharpbq-reader (strm c n)
+  (declare (ignore c))
+  (let ((expr (funcall (get-macro-character #\`) strm nil)))
+    `(lambda ,(if n
+                (map0-n (lambda (n) (intern (format nil "A~A" n))) (1- n))
+                (anaphora-list expr))
+       ,expr)))
+
+;;; Try it out by register it as #?
+(set-dispatch-macro-character #\# #\? #'sharpbq-reader)
+
+(read-from-string "#?(print (list ,a0 ,a1 ,a3))")
+(read-from-string "#4?(print (list ,a0 a1 a3))")
