a8e1080274e64e07088f145614fe1c61dbf1908d
chiku <samugari@is.s.u-tokyo.ac.jp>
2015-09-18 00:45:48 +0900

    A new anaphora collecting algorithm using local reader macro.
    The new #` reader macro first READs one form that follows to #` with
    establishing a non-terminating reader macro for #\a. The function for
    character #\a READs one another form and registers a symbol if the read
    object was written in decimal, or it behaves as if it was an alphadigit
    character.
    
    This approach avoids to destroy the structure and as a result it
    preserves the semantics. Although the current implementation avoids to
    collect the anaphoras in the nested #` reader macro, the implmenation
    which collects them is also easy. The important is, the semantics.
    
    There are a few known-problems. First, The local reader macro is only
    set to #\a, but naturally #\A should also be set the same reader macro
    function in typical use case. It should be decided based on the value of
    (readtable-case *readtable*).
    
    Second, the check about the suffix part of character #\a is too simple
    and poor. As it was stated in the deleted part, anaphora should be the
    member of follwing regular language:
    
        A[1-9][0-9]* | A0
    
    Padding 0 should not be allowed because it is not clear which should be
    former if both ``A0'' and ``A00'' weer used simultaneously. Moreover,
    current check accepts ``A1D"" as an anaphora.
    
    Third, this reader macro function makes a copy of current readtable.
    Typically, its some sort of structure that has slots for all 128 base
    characters and properties such as readtable-case. The cost for copy is
    not small enough to ignore.

--- sharp-backquote.lisp
+++ sharp-backquote.lisp
@@ -4,66 +4,34 @@
 ;;; preferable.
 (in-package :sharp-backquote)
 
-(defun anaphorap (sym)
-  " Retruns T if the name of the given symbol ``SYM'' is
-   a member of following regular language:
-   A[1-9][0-9]*|A0
-   "
-  (let ((name (symbol-name sym)))
-    (and (<= 2 (length name))
-         (string= (take name) "A")
-         (every #'digit-char-p (drop name))
-         (or (= (length name) 2)
-             ;; Disables padding 0
-             (not (char= (elt name 1) #\0))))))
-
-;;; For example, Clozure CL does not need this function. Unquoted
-;;; expressions are not something special structure.
-(defun unquotedp (obj)
-  #+sbcl
-  (if (sb-impl::comma-p obj)
-    (values t (sb-impl::comma-expr obj))))
-
-(mapcar #'unquotedp (flatten (read-from-string "'(lambda (x) `(print ,a))")))
-
-(mapcar #'unquotedp (flatten (read-from-string "'(lambda (x) ``(print ,,a))")))
-
-(mapcar #'unquotedp (flatten (read-from-string "(lambda (x) `(print ,(`,a)))")))
-
-(defun anaphora-list (tree)
-  " Returns a list that is composed of symbols whose ANAPHORAP check is
-   T. Each symbol appears only once in the list and the list is sorted
-   by the name.
-   "
-  (labels ((collect (tree)
-             (filter (lambda (obj)
-                       (or (and (symbolp obj) (anaphorap obj) obj)
-                           #+sbcl
-                           (multiple-value-bind (? expr)
-                             (unquotedp obj)
-                             (and ? (collect expr)))))
-                     (flatten tree))))
-    (sort (remove-duplicates (flatten (collect tree)))
-          #'string<= :key #'symbol-name)))
-
-(anaphora-list (read-from-string "(lambda (x) `(print ,(`,a0)))"))
-
-(anaphora-list (read-from-string "`(print (list ,a0 ,a1 ,a3))"))
-
 (let ((bqreader-fn (get-macro-character #\` (copy-readtable nil))))
-  (defun |#?-reader| (strm c n)
+  (defun sharp-backquote-reader (strm c n)
     (declare (ignore c))
-    (let ((expr (funcall bqreader-fn strm nil)))
-      `(lambda ,(if n
-                  (loop :for i :from 0 :upto (1- n)
-                        :collect (intern (format nil "A~A" i)))
-                  (anaphora-list expr))
-         ,expr))))
+    (if n
+      `(lambda ,(loop :for i :from 0 :upto (1- n)
+                      :collect (intern (format nil "A~D" i)))
+         ,(funcall bqreader-fn strm nil))
+      (let ((*readtable* (copy-readtable))
+            anaphoras)
+        (set-macro-character
+          #\a (lambda (strm c)
+                (declare (ignore c))
+                ;; Here, base must be 10. The definition of anaphora is
+                ;; the basis Even if *READ-BASE* is not 10.
+                (if (digit-char-p (peek-char nil strm t nil t))
+                  (car (pushnew (intern (format nil "A~D" (read strm t nil t)))
+                                anaphoras))))
+          t)
+        (let ((expr (funcall bqreader-fn strm nil)))
+          `(lambda ,(sort anaphoras #'string<= :key #'symbol-name)
+             ,expr))))))
 
-(set-dispatch-macro-character #\# #\? #'|#?-reader|)
+(set-dispatch-macro-character #\# #\` #'sharp-backquote-reader)
 
-(read-from-string "#?(print (list ,a0 ,a1 ,a3))")
-(read-from-string "#4?(print (list ,a0 a1 a3))")
-(read-from-string "#0?(print (list ,a0 a1 a3))")
-(read-from-string "#?(print (list ,a0 a1 a3))")
-(read-from-string "#?(print (list ,a0 ,a1 ,(progn `(foo ,a0) `(bar ,a2))))")
+(read-from-string "#`(print (list ,a0 ,a1 ,a3))")
+(read-from-string "#4`(print (list ,a0 a1 a3))")
+(read-from-string "#0`(print (list ,a0 a1 a3))")
+(read-from-string "#`(print (list ,a0 a1 a3))")
+(read-from-string "#`(print (list ,a0 ,a1 ,(progn `(foo ,a0) `(bar ,a2))))")
+(read-from-string "#`(print (list ,a0 ,a1 ,(progn #`(foo ,a4) `(bar ,a2))))")
+(read-from-string "#`(print (list ,a0 ,a1 ,(progn #`(foo ,a4) #`(bar ,a2))))")
