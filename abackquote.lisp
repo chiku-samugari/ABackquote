76e7fdedec215d7c05153d6abdc3a8785e7a7069
chiku <samugari@is.s.u-tokyo.ac.jp>
2015-09-20 22:27:10 +0900

    Anaphora detection algorithm is improved.

--- abackquote.lisp
+++ abackquote.lisp
@@ -4,11 +4,27 @@
 ;;; preferable.
 (in-package :abackquote)
 
-(defun anaphora-suffix-p (strm)
+(defun terminating-character-p (ch)
+  (or (in ch #\Tab #\Newline #\Linefeed #\Page #\Return #\Space)
+      ;;; Actually speaking, TERMINATING-MACRO-CHARACTER-P is desirable
+      ;;; here.  But it is conceptually impossible in Common Lisp
+      ;;; because Common Lisp does not offer any way to check if a macro
+      ;;; character is terminating or non-terminating.
+      (get-macro-character ch)))
+
+(defun read-suffix (strm)
   ;; Here, base must be 10. The definition of anaphora is the basis.
   ;; Even if *READ-BASE* is not 10.
-  ;; TODO: FIXME!
-  (digit-char-p (peek-char nil strm t nil t)))
+  (do (clst
+        (ch (peek-char nil strm t nil t) (peek-char nil strm t nil t)))
+    ((not (digit-char-p ch))
+     ;; TODO: what about a1\\1? Should it be an anaphora?
+     (cond ((terminating-character-p ch)
+             (if clst
+               (values (coerce (nreverse clst) 'string) t)
+               (values "" nil)))
+           (t (values (coerce (nreverse clst) 'string) nil))))
+    (push (read-char strm nil t nil) clst)))
 
 (defun intern-anaphora (asuffix)
   (intern (format nil "A~D" asuffix)))
@@ -25,13 +41,14 @@
         (set-macro-character
           #\a (lambda (strm c)
                 (declare (ignore c))
-                (if (anaphora-suffix-p strm)
-                  (car (pushnew (intern-anaphora (read strm t nil t))
-                                anaphoras))
-                  (let ((fake (make-concatenated-stream
-                                (make-string-input-stream "\\A")
-                                strm)))
-                    (read fake t nil t))))
+                (multiple-value-bind (suffix anaphora?)
+                  (read-suffix strm)
+                  (if anaphora?
+                    (car (pushnew (intern-anaphora suffix) anaphoras))
+                    (let ((fake (make-concatenated-stream
+                                  (make-string-input-stream (concat-str "\\A" suffix))
+                                  strm)))
+                      (read fake t nil t)))))
           t)
         (let ((expr (funcall bqreader-fn strm nil)))
           `(lambda ,(sort anaphoras #'string<= :key #'symbol-name)
@@ -47,4 +64,11 @@
 (read-from-string "#`(print (list ,a0 ,a1 ,(progn #`(foo ,a4) `(bar ,a2))))")
 (read-from-string "#`(print (list ,a0 ,a1 ,(progn #`(foo ,a4) #`(bar ,a2))))")
 (read-from-string "#`(print (list ,abc ,a1 ,a3))")
-(read-from-string "#`(print (list ,a (bc ,a2) ,a1 ,a0))")
+(let ((*read-base* 4))
+  (read-from-string "#`(print (list ,a10 (bc ,a2) ,a1 ,a0 a4))"))
+(read-from-string "#`(print (list ,a2bc ,a1 ,a3))")
+
+(read-from-string "#`(list ,a0)")
+(read-from-string "#`(list ,a1 ,a0)")
+(read-from-string "#`(list ,a\\1 ,a0)")
+(read-from-string "#`(list ,a1\\1 ,a0)")
