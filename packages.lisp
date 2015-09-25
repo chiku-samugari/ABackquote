7d79309615c26b500a9cf0d4c8de780e0f4610d2
chiku <samugari@is.s.u-tokyo.ac.jp>
2015-09-25 23:11:42 +0900

    [Tiny] Unused package UTIL is removed.

--- packages.lisp
+++ packages.lisp
@@ -1,7 +1,3 @@
-(defpackage :util
-  (:use :cl)
-  (:export :let-it-be :with-gensyms :it :concat-str :awhile :last1 :in))
-
 (defpackage :abackquote
-  (:use :cl :util)
+  (:use :cl)
   (:export :enable-abackquote))
